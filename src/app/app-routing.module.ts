7eff30a1a3c1023aae191f925338b0ef75391eb7
Bayoudyasmine <bayoud.yas@gmail.com>
2024-06-09 15:15:56 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { CreditorsListComponent } from './creditors-list/creditors-list.component';
-import { HistoryComponent } from './history/history.component';
-import {FormulaireComponent } from './formulaire/formulaire.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 import { CreatePaymentAccountComponent } from './create-payment-account/create-payment-account.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/creditors-list', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'creditors-list', component: CreditorsListComponent },
-  { path: 'history', component: HistoryComponent },
-  {path : 'formulaire', component: FormulaireComponent },
-  {path : 'create-payment-account', component: CreatePaymentAccountComponent },
+  { path: 'formulaire', component: FormulaireComponent },
+  { path: 'create-payment-account', component: CreatePaymentAccountComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [provideRouter(routes, withComponentInputBinding())],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule } from '@angular/forms'; // Assurez-vous que ReactiveFormsModule est importé
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatePaymentAccountComponent } from './create-payment-account/create-payment-account.component';
 import { FormulaireComponent } from './formulaire/formulaire.component';
-import {CreditorsListComponent} from "./creditors-list/creditors-list.component";
-import {HistoryComponent} from "./history/history.component";
-import { MatTabsModule } from '@angular/material/tabs'
-@NgModule({
-  declarations: [
-    AppComponent,
-    FormulaireComponent,
-    CreatePaymentAccountComponent,
-    CreditorsListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    MatTabsModule,
-    FormsModule
-  ],
-  providers: [],
-  exports: [
-    CreditorsListComponent,
-    FormulaireComponent,
-    CreatePaymentAccountComponent
+import { CreditorsListComponent } from './creditors-list/creditors-list.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-  ],
-  bootstrap: [AppComponent]
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        FormulaireComponent,
+        CreatePaymentAccountComponent,
+        CreditorsListComponent,
+        HomeComponent,
+        NavbarComponent,
+
+
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        MatTabsModule,
+        FormsModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
